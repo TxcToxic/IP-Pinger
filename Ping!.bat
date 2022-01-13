30df5ff728fa2ec8e291cc01e39a61fe0d02c69e
TxcToxic <txctoxic023@gmail.com>
2022-01-13 03:51:30 +0100

    IP Pinger
    This is a CMD batch tool to ping IP´s

---
+++ Ping!.bat
@@ -0,0 +1,50 @@
+echo off
+title Pinging Console! (ping -t IP/URL)
+color 4
+cls
+color 1
+tree
+color 2
+tree
+color 3
+dir /s
+color 4
+dir /s
+color 5
+tree
+color b
+color 1
+tree
+color 2
+tree
+color 3
+dir /s
+color 4
+tree
+color 5
+dir /s
+color b
+color 1
+tree
+color 2
+tree
+color 3
+dir /s
+color 4
+tree
+color 5
+tree
+color b
+color 1
+dir /s
+color 2
+tree
+color 3
+tree
+color 4
+tree
+color 5
+dir /s
+color 4
+cls
+ping -t 127.0.0.1
